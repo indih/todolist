57ed56ece1241357ecbf6e328e49dc9b5cdb3017
indih <indih1232@gmail.com>
2019-11-07 01:13:38 +0800

    add data and function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,51 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'todolist';
+  close = '\u00D7';
+  inputtitle = '';
+  lists = [
+  {
+    'title':'Hit the gym',
+    'check': false
+  },
+  {
+    'title':'Pay bills',
+    'check':true
+  },
+  {
+    'title':'Meet George',
+    'check':false
+  },
+  {
+    'title':'Buy eggs',
+    'check':false
+  },
+  {
+    'title':'Read a book',
+    'check':false
+  },
+  {
+    'title':'Organize office',
+    'check':false
+  }];
+  addTitle(): void {
+    if(this.inputtitle == '')
+    {
+      alert("You must write something!");
+    }
+    else
+    {
+      this.lists.push({'title':this.inputtitle,'check':false});
+      this.inputtitle = '';
+    }
+  }
+  completeTitle(index: number): void {
+    this.lists[index].check = !this.lists[index].check;
+  }
+  removeTitle(index: number): void {
+    this.lists.splice(index,1);
+  }
 }
